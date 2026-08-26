c7d0f62021e5d0d0901933a57b0746969f502d7f
eren-xyz <erengoktugbolat@gmail.com>
2026-08-26 23:42:17 +0200

    Added autoscale plugin

--- script.js
+++ script.js
@@ -6,3 +6,26 @@ enterScreen.addEventListener("click", () => {
     // Start the website animations
     document.body.classList.add("entered");
 });
+
+
+var ratio;
+var left;
+resize();
+
+$(window).resize(function () {resize();});
+
+function resize()
+{
+    ratio = window.innerHeight / $('body').innerHeight();
+    if (window.innerWidth / $('body').innerWidth() < ratio) {
+        ratio = window.innerWidth / $('body').innerWidth();
+    }
+    ratio -= .04;
+    $('body').css('-ms-zoom', ratio);
+    $('body').css('-moz-transform', 'scale(' + ratio + ')');
+    $('body').css('-o-transform', 'scale(' + ratio + ')');
+    $('body').css('-webkit-transform', 'scale(' + ratio + ')');
+    $('body').css('transform', 'scale(' + ratio + ')');
+    left = ($(window).innerWidth() - $('body').outerWidth() * ratio) / 2;
+    $('body').css('left', left);
+}
